bb395358479993172e032c2cc0d5140c238edb28
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-26 12:06:40 +0100

    adding a double

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../../app/lib/card'
 
 feature 'card' do 
+  # creating a double
+  let(:deck){double :deck,:count_cards=>52}
+
   scenario "can have a card" do 
     card = Card.new('h',2)
     expect(card.rank).to eq 2
@@ -10,5 +13,10 @@ feature 'card' do
     card1 = Card.new('h',2)
     card2 = Card.new('c',2)
     expect(card1.isRankEqual(card2)).to be true
+  end
+
+  scenario 'should have 52 cards in a deck' do 
+
+    expect(deck.count_cards).to eq(52)
   end
 end
