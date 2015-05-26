83c2acda4bd8b4f4a62f81fc2a7a98c784a3702d
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-26 23:09:57 +0100

    finished with hand

--- app/lib/hand.rb
+++ app/lib/hand.rb
@@ -19,4 +19,31 @@ class Hand
   def lost?
     @score > 21
   end
+
+  def numOfCards
+    @cards.length
+  end
+
+  def halfScore
+    @score /= 2
+  end
+
+  def setScore(score)
+    @score = score
+  end
+
+  def isSplittingAllowed
+    return false if cards.length > 2
+    return @cards[0].isRankEqual(@cards[1]) if cards.length == 2
+    false
+  end
+
+  def popCard
+    @cards.pop
+  end
+
+  def toArrayOfStrings
+    return [] if @cards.length == 0
+    return @cards.map{|card| card.toString}
+  end
 end
--- spec/features/hand_spec.rb
+++ spec/features/hand_spec.rb
@@ -2,8 +2,9 @@ require './app/lib/hand'
 
 describe 'hand' do
   let(:hand){Hand.new}
-  let(:card1){double :card1,:rank=>10}
-  let(:card2){double :card2,:rank=>5}
+  let(:card1){double :card1,:rank=>10,:isRankEqual=>true}
+  let(:card2){double :card2,:rank=>5,:isRankEqual=>false}
+  let(:card3){double :card3,:rank=>10}
   it 'should start with score of zero' do 
     expect(hand.cards.size).to eq 0
   end
@@ -21,4 +22,40 @@ describe 'hand' do
   it 'should know if lost' do 
     expect(hand.lost?).to be false
   end
+
+  it 'should know how many cards are in teh hand' do 
+    hand.add(card1)
+    hand.add(card2)
+    expect(hand.numOfCards).to eq(2)
+  end
+
+  it 'should split the points' do 
+    hand.add(card1)
+    hand.add(card3)
+    hand.halfScore
+    expect(hand.score).to equal(10)
+  end
+
+  it 'should set a score' do 
+    hand.setScore(20)
+    expect(hand.score).to eq(20)
+  end
+
+  it 'should check if spliting is allowed' do 
+    hand.add(card2)
+    hand.add(card1)
+    expect(hand.isSplittingAllowed).to be false
+  end
+
+  it 'should check if spliting is allowed' do 
+    hand.add(card1)
+    hand.add(card3)
+    expect(hand.isSplittingAllowed).to be true
+  end
+
+  it 'should be able to pop a cards if split' do 
+    hand.add(card1)
+    hand.add(card3)
+    expect(hand.popCard).to eq(card3)    
+  end 
 end
