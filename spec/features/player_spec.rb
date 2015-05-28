be1c883da55d481347d595264ccbcea14ae2302f
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-28 09:51:09 +0100

    adding player

--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -2,7 +2,10 @@ require './app/lib/player'
 
 describe 'player options' do 
   let(:player){Player.new}
+  card(:card){double :card}
   it 'should have one hand initially' do 
     expect(player.hands.length).to eq(1)
   end
+
+
 end
