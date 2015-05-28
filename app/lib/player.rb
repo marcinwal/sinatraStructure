ec2b796aa4d7121802e83b5e903ca1d7e8ded7b3
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-28 18:46:52 +0100

    adding card to the hand

--- app/lib/player.rb
+++ app/lib/player.rb
@@ -5,4 +5,11 @@ class Player
     @hands[0] = Hand.new
     @option = ''
   end
+
+  def addCard(card,hand = 0)
+    @hands[hand].add(card)
+    @option = 'hit'
+  end
+
+
 end
--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -2,10 +2,14 @@ require './app/lib/player'
 
 describe 'player options' do 
   let(:player){Player.new}
-  card(:card){double :card}
+  let(:card){double :card,:rank=>2,:color=>2}
   it 'should have one hand initially' do 
     expect(player.hands.length).to eq(1)
   end
 
+  it 'should be able to add a card' do 
+    player.addCard(card)
+    expect(player.option).to eq('hit')
+  end
 
 end
