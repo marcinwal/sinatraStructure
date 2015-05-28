0a93bc55720cfd1955f491e52e0a2c60f1b5338d
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-28 18:54:59 +0100

    splitting the cards

--- app/lib/player.rb
+++ app/lib/player.rb
@@ -11,5 +11,17 @@ class Player
     @option = 'hit'
   end
 
+  def lost?
+    return @hands[0].lost? unless @hands[1]
+    return @hands[0].lost? && hands[1].lost?
+  end
+
+  def splitHand
+    @hands[1] = Hand.new
+    @hands[1].add(@hands[0].popCard)
+    @hands[0].halfScore
+    @hands[1].setScore(@hands[0].score)
+    @option = 'split'
+  end
 
 end
--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -3,6 +3,7 @@ require './app/lib/player'
 describe 'player options' do 
   let(:player){Player.new}
   let(:card){double :card,:rank=>2,:color=>2}
+  let(:card2){double :card2,:rank=>2,:color=>2}
   it 'should have one hand initially' do 
     expect(player.hands.length).to eq(1)
   end
@@ -10,6 +11,18 @@ describe 'player options' do
   it 'should be able to add a card' do 
     player.addCard(card)
     expect(player.option).to eq('hit')
+  end
+
+  it 'should be able to tell if lost' do 
+    expect(player.lost?).to be false
+  end
+
+  it 'should be able to split hands' do
+    player.addCard(card)
+    player.addCard(card2) 
+    player.splitHand
+    expect(player.hands.length).to eq 2
+    expect(player.option).to eq 'split'
   end
 
 end
