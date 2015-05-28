ffac665c65d4fa139bde1ef2e1c7f1c334af9236
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-28 19:20:07 +0100

    finished with player

--- app/lib/player.rb
+++ app/lib/player.rb
@@ -24,4 +24,35 @@ class Player
     @option = 'split'
   end
 
+  def isSplittingAllowed
+    return false if @hands.length > 1
+    return @hands[0].isSplittingAllowed
+  end
+
+  def stand(hand = 0)
+    @option = 'stand'+hand.to_s
+  end
+  #changing the logic in game for
+  #two hands
+  def score(hand = 0)
+    return @hands[hand].score if @hands[hand]
+  end
+
+  def handsToArrayOfString
+    return [@hands[0].toArrayOfStrings,@hands[1].handsToArrayOfStrings] if @hands[1]
+    return @hands[0].toArrayOfStrings
+  end
+
+  def hasTwoHands
+    return true if @hands[1]
+    return false
+  end
+
+  def isHandFinished(hand)
+    return true if hand == 0 and @option == 'stand0'
+    return true if hand == 1 and @option == 'stand1'
+    return @hands[hand].lost?
+  end
+
+
 end
--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -2,7 +2,8 @@ require './app/lib/player'
 
 describe 'player options' do 
   let(:player){Player.new}
-  let(:card){double :card,:rank=>2,:color=>2}
+  let(:card){double :card,:rank=>2,:color=>2,
+      :isRankEqual=>true,:toString=>'h2'}
   let(:card2){double :card2,:rank=>2,:color=>2}
   it 'should have one hand initially' do 
     expect(player.hands.length).to eq(1)
@@ -23,6 +24,42 @@ describe 'player options' do
     player.splitHand
     expect(player.hands.length).to eq 2
     expect(player.option).to eq 'split'
+  end
+
+  it 'should tell if splitting is allowed' do 
+    player.addCard(card)
+    player.addCard(card2) 
+    expect(player.isSplittingAllowed).to be true
+  end
+
+  it 'should be possible to stand' do 
+    player.stand
+    expect(player.option).to eq 'stand0'
+  end 
+
+  it 'should calc the score' do 
+    expect(player.score).to eq 0 
+    player.addCard(card)
+    expect(player.score).to eq 2
+  end
+
+  it 'should convert hands to string' do 
+    player.addCard(card)
+    expect(player.handsToArrayOfString).to eq ['h2']
+  end
+
+  it 'should know how many hands' do 
+    player.addCard(card)
+    player.addCard(card2)
+    expect(player.hasTwoHands).to be false
+    player.splitHand
+    expect(player.hasTwoHands).to be true
+  end
+
+  it 'should know if a hand is finished' do 
+    player.addCard(card)
+    player.stand
+    expect(player.isHandFinished(0)).to be true
   end
 
 end
