ab4a36288af0c8708e78dd6d7b78e72f0a7c745d
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-29 09:01:46 +0100

    starting with a dealer

--- app/lib/dealer.rb
+++ app/lib/dealer.rb
@@ -1,2 +1,8 @@
-class Dealer
+require_relative './player'
+
+class Dealer < Player
+
+  def shouldTakeCard
+    return @hands[0].score <=16 ? true : false
+  end
 end 
--- spec/features/dealer_spec.rb
+++ spec/features/dealer_spec.rb
@@ -2,4 +2,8 @@ require './app/lib/dealer'
 
 describe 'the dealer' do 
   let(:dealer){Dealer.new}
+
+  it 'should know if to take a card' do 
+    expect(dealer.shouldTakeCard).to be true
+  end
 end
